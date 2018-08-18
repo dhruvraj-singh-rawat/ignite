b895feccd5c77b4a7a706ab64ca2238df229c7e5
Dhruvraj Rawat <dhruvraj-singh-rawat@users.noreply.github.com>
2018-08-18 20:09:01 +0530

    Finesh :100:

--- first.py
+++ first.py
@@ -2,6 +2,12 @@ import random # Used to Generate Random Number
 
 random_arr=[] # Creating an Empty Array
 for x in range(1000000):  
-  random_arr.append(random.randint(1,101)) # Appending Random Number Generate in range of (1,100) to the Array
+  random_arr.append(random.randint(1,100)) # Appending Random Number Generate in range of (1,100) to the Array
 
-pr
+
+count_arr=[0] * 100
+for i in range(len(random_arr)):
+  
+  count_arr[random_arr[i] -1 ]=count_arr[random_arr[i] -1 ]+1
+
+print (count_arr)
