8f0194eba111892546c130af2a96bde0a67a2563
Dhruvraj Rawat <dhruvraj-singh-rawat@users.noreply.github.com>
2018-08-18 19:51:29 +0530

    Q1 : Initial Commit

---
+++ first.py
@@ -0,0 +1,7 @@
+import random # Used to Generate Random Number
+
+random_arr=[] # Creating an Empty Array
+for x in range(1000000):  
+  random_arr.append(random.randint(1,101)) # Appending Random Number Generate in range of (1,100) to the Array
+
+pr
