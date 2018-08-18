b2a99b53bca9efbe21ce304249856ce870bdf1e9
Dhruvraj Rawat <dhruvraj-singh-rawat@users.noreply.github.com>
2018-08-18 21:47:22 +0530

    Added Comments

--- first.py
+++ first.py
@@ -1,13 +1,22 @@
 import random # Used to Generate Random Number
 
-random_arr=[] # Creating an Empty Array
+
+# Creating an Random Array of Size 1M cointaing Elements between < 1 To 100 >
+random_arr=[] 
 for x in range(1000000):  
   random_arr.append(random.randint(1,100)) # Appending Random Number Generate in range of (1,100) to the Array
 
+
+####
+#   Main Algorithm : An array of Size 100 is initialized and as the random number comes the Count of that Index is Incremented
 
 count_arr=[0] * 100
 for i in range(len(random_arr)):
   
   count_arr[random_arr[i] -1 ]=count_arr[random_arr[i] -1 ]+1
+####
 
-print (count_arr)
+# Printing the Solution
+for i in range(100):
+  print("Total Number of ", i+1, " are :",count_arr[i])
+
